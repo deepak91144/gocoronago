7abfe888d27019c1d059427814aa168f9f58eaea
deepak <iamdj1111@gmail.com>
2020-09-10 02:39:43 +0530

    Updates

---
+++ precache-manifest.0b523d82a497f49991749965bb1f0290.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15485d12f7b633a406762faa3e2d665",
+    "url": "/gocoronago/index.html"
+  },
+  {
+    "revision": "946d48b8498af6775512",
+    "url": "/gocoronago/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a35bbcd30183fd92272f",
+    "url": "/gocoronago/static/css/main.0027a5a5.chunk.css"
+  },
+  {
+    "revision": "946d48b8498af6775512",
+    "url": "/gocoronago/static/js/2.57daae5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gocoronago/static/js/2.57daae5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35bbcd30183fd92272f",
+    "url": "/gocoronago/static/js/main.2b2c3a6e.chunk.js"
+  },
+  {
+    "revision": "f2557fee7fb695a5dc0c",
+    "url": "/gocoronago/static/js/runtime-main.003eff6a.js"
+  },
+  {
+    "revision": "9090517e04fff530af0ae1030e4a0454",
+    "url": "/gocoronago/static/media/corona.9090517e.jpg"
+  }
+]);
